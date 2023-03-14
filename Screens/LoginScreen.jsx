d87272dcef85c7ac0c2d0b0ea7d2547dabc8c5ff
Bogdan <bogdanberezka@gmail.com>
2023-03-14 13:20:02 +0200

    make-login-form

---
+++ Screens/LoginScreen.jsx
@@ -0,0 +1,184 @@
+// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+
+// import { useFonts } from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+// SplashScreen.preventAutoHideAsync();
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+export default function LoginScreen() {
+  // console.log("Тип платформи з якої переглядається сторінка: ", Platform.OS);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  const handleSubmit = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  // const [fontsLoaded] = useFonts({
+  //   "Roboto-Regular": require("../assets/fonts/Roboto-Regular.ttf"),
+  // });
+
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/photoBackground.jpg")}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : ""}
+          >
+            <View style={styles.formBackgound}>
+              <View
+                style={{
+                  ...styles.form,
+                  paddingBottom: isShowKeyboard ? 20 : 100,
+                }}
+              >
+                <Text style={styles.title}>Ввійти</Text>
+                <View>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Адреса електронної пошти"
+                    value={state.email}
+                    onFocus={() => {
+                      setIsShowKeyboard(true);
+                    }}
+                    onChangeText={(value) =>
+                      setState((prevState) => ({ ...prevState, email: value }))
+                    }
+                  />
+                </View>
+                <View style={{ marginTop: 16 }}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Пароль"
+                    secureTextEntry={true}
+                    value={state.password}
+                    onFocus={() => {
+                      setIsShowKeyboard(true);
+                    }}
+                    onChangeText={(value) =>
+                      setState((prevState) => ({
+                        ...prevState,
+                        password: value,
+                      }))
+                    }
+                  />
+                </View>
+                <TouchableOpacity
+                  activeOpacity={0.7}
+                  style={styles.btn}
+                  onPress={handleSubmit}
+                >
+                  <Text style={styles.btnTitle}>Ввійти</Text>
+                </TouchableOpacity>
+                <Text style={styles.toRegister}>
+                  Немає аккаунта? Зареєструватися
+                </Text>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    // alignItems: "center",
+  },
+  formBackgound: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    // paddingBottom: 100,
+  },
+  form: {
+    marginHorizontal: 16,
+    paddingTop: 32,
+    // paddingBottom: 100,
+  },
+  title: {
+    color: "#212121",
+    fontSize: 30,
+    fontWeight: 500,
+    textAlign: "center",
+    marginBottom: 33,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    height: 50,
+    color: "#212121",
+    fontSize: 16,
+    padding: 16,
+  },
+  btn: {
+    backgroundColor: Platform.OS === "ios" ? "#4169e1" : "#FF6C00",
+    height: 50,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 43,
+    ...Platform.select({
+      ios: {
+        backgroundColor: "#4169e1",
+      },
+      android: {
+        backgroundColor: "#FF6C00",
+      },
+      default: {
+        backgroundColor: "green",
+      },
+    }),
+  },
+  btnTitle: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  toRegister: {
+    color: "#1B4371",
+    fontSize: 18,
+    textAlign: "center",
+    marginTop: 16,
+  },
+});
